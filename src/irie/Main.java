37c27a9b1f0f05d771511573039e0d998d4eb40f
kiibabe <kisetsuirie@gmail.com>
2021-05-21 10:02:57 +0900

    Added for the stretch challenge.

--- src/irie/Main.java
+++ src/irie/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, my name is Kisetsu.");
         System.out.println("こんにちは Kisetsu、私の名前はベンです。");
+        System.out.println("This is a test for the stretch challenge");
+
     }
 }
